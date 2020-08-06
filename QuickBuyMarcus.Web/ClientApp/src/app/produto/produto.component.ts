d08e2225caa77aa42356b7a9ddce4816d6fdc9e8
marcus.oliveira <marcuspaulo56@yahoo.com>
2020-08-06 16:09:11 -0300

    Iniciando a criação do componente de login

--- QuickBuyMarcus.Web/ClientApp/src/app/produto/produto.component.ts
+++ QuickBuyMarcus.Web/ClientApp/src/app/produto/produto.component.ts
@@ -1,4 +1,12 @@
+import { Component } from "@angular/core"
+
+@Component({
+  selector: "app-produto",
+  template : "<html><body>{{ obterNome() }}</body></html>"
+})
+
 /* Nome das classes em maiusculo - Pascal Case */
+
 export class ProdutoComponent {
   /*Camel case para variaveis, atributos e nomes das funções*/
   public nome: string;
@@ -6,6 +14,6 @@ export class ProdutoComponent {
 
 
   public obterNome(): string {
-    return this.nome;
+    return "Samsung";
   }
 }
