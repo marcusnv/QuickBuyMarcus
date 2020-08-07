b6485cfaacc9e67af803d88a0acace09b383aa49
marcus.oliveira <marcuspaulo56@yahoo.com>
2020-08-07 16:51:23 -0300

    Incluindo mensagens validação no bootstrap e alterações na tela de login

--- QuickBuyMarcus.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuyMarcus.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Usuario } from "../../modelo/usuario";
 
 @Component({
   selector: "app-login",
@@ -6,6 +7,18 @@ import { Component } from "@angular/core";
   styleUrls: ["./login.component.css"]
 })
 export class LoginComponent {
-  public email = "marcus@teste.com";
-  public senha = "12345";
+  public usuario;
+
+  constructor() {
+    this.usuario = new Usuario();
+  }
+
+  entrar() {
+
+    }
+
+  public enderecoImagem = "https://img2.gratispng.com/20180727/sfz/kisspng-logo-best-buy-brand-bouy-5b5be347df40a9.7352804615327486159145.jpg";
+  public titulo = "BEST BUY Marcus";
+
 }
+
