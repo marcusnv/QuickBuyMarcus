2981eed8f523e169178d1b726a6c10a7c12225fe
marcus.oliveira <marcuspaulo56@yahoo.com>
2020-08-10 13:13:11 -0300

    Alterações entrar/sair usuario

--- QuickBuyMarcus.Web/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ QuickBuyMarcus.Web/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-menu',
@@ -8,11 +9,23 @@ import { Component } from '@angular/core';
 export class NavMenuComponent {
   isExpanded = false;
 
+  constructor(private router: Router) {
+
+  }
+
   collapse() {
     this.isExpanded = false;
   }
 
   toggle() {
     this.isExpanded = !this.isExpanded;
+  }
+
+  public usuarioLogado(): boolean {
+    return sessionStorage.getItem("usuario-autenticado") == "1";
+  }
+  sair() {
+    sessionStorage.setItem("usuario-autenticado", "");
+    this.router.navigate(['']);
   }
 }
--- QuickBuyMarcus.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuyMarcus.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { Usuario } from "../../modelo/usuario";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-login",
@@ -9,12 +10,15 @@ import { Usuario } from "../../modelo/usuario";
 export class LoginComponent {
   public usuario;
 
-  constructor() {
+  constructor(private router: Router) {
     this.usuario = new Usuario();
   }
 
   entrar() {
-
+    if (this.usuario.email == "teste@teste.com.br" && this.usuario.senha == "123456") {
+      sessionStorage.setItem("usuario-autenticado", "1");
+      this.router.navigate(['/'])
+    }
     }
 
   public enderecoImagem = "https://img2.gratispng.com/20180727/sfz/kisspng-logo-best-buy-brand-bouy-5b5be347df40a9.7352804615327486159145.jpg";
